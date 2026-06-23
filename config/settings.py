dc3d70eb87a3c668d273b4b35c61822364da95cf
RiyadSheikh27 <riyad.cse27@gmail.com>
2026-06-23 09:48:57 +0600

    LATEST

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# Production Database Configuration (PostgreSQL)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cpmo_db',
+        'USER': 'cpmo_user',
+        'PASSWORD': 'StrongPassword123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
